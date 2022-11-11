2ea4f33c5b10a1ea8eac4b3ee2bf3b19fca6a54c
sny430 <shinya430.g@gmail.com>
2022-11-12 00:58:35 +0900

    :sparkles: add ls.py and ls_logy.py

---
+++ ls_logy.py
@@ -0,0 +1,46 @@
+#最小二乗法計算機 y軸片対数ver
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def reg1dim(x, y):
+    n = len(x)
+    a = ((np.dot(x, y)- y.sum() * x.sum()/n)/
+        ((x ** 2).sum() - x.sum()**2 / n))
+    b = (y.sum() - a * x.sum())/n
+    return a, b
+
+if __name__ == "__main__":
+    x = []
+    y = []
+    yl = []
+
+    plt_title = str(input("Title: "))
+    x_label = str(input("x label: "))
+    y_label = str(input("y label: "))
+    elem_num = int(input("Number of Elements: "))
+
+    for i in range(elem_num):
+        v = input("x-{}: ".format(i))
+        x.append(v)
+        v = input("y-{}: ".format(i))
+        y.append(v)
+        yl.append(math.log10(float(v)))
+
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
+    yl = np.array(yl, dtype=float)
+    a, b = reg1dim(x, yl)
+
+    print("[i] inclination: " + str(a))
+    print("[i] intercept: " + str(b))
+    fig, ax = plt.subplots()
+    ax.set(title=plt_title, xlabel=x_label, ylabel=y_label)
+    ax.set_yscale('log')
+    plt.scatter(x, y, color="k")
+    plt.plot([0, x.max()], [pow(10, b), pow(10, a * x.max() + b)])
+    ax.minorticks_on()
+    ax.grid(which='major', color='gray', linestyle='-')
+    ax.grid(which='minor', color='gray', linestyle='dotted')
+    plt.show()
