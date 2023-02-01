d688ecb99fea871776e4da994ca9bfcbe0d716df
sny430 <shinya430.g@gmail.com>
2023-02-01 17:19:05 +0900

    :sparkles: add linear plotter

---
+++ linear.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    
+    inclinations = []
+    plt_title = str(input("Title: "))
+    x_label = str(input("x label: "))
+    y_label = str(input("y label: "))
+    x_max = float(input("x max: "))
+    elem_num = int(input("Number of Elements: "))
+    
+    for i in range(elem_num):
+        v = input("inclination-{}: ".format(i))
+        inclinations.append(v)
+    
+    fig, ax = plt.subplots()
+    ax.set(title=plt_title, xlabel=x_label, ylabel=y_label)
+    for i in range(elem_num):
+        plt.plot([0, x_max], [0, float(inclinations[i]) * x_max])
+    ax.minorticks_on()
+    ax.grid(which='major', color='gray', linestyle='dotted')
+    plt.show()
